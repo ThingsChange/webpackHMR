e2ee63205299cae8f19bf3f81adcf7ba920b54b1
Hendrik Eng <hendrik@engereauswahl.de>
2017-03-01 13:23:35 +0100

    init

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,4 @@
 // import CSS to bundle it with webpack
 import css from '../scss/app.scss'; // eslint-disable-line
 
-document.write('needs to do full reload.');
+document.write('needs to do full reload if js gets changed.');
